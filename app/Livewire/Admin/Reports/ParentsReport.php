b151d44c7df4ad89a2d8d703090e412660d1be18
AfsalTellizo <afsal@blarve.com>
2025-01-27 12:06:46 +0530

    parents report

--- app/Livewire/Admin/Reports/ParentsReport.php
+++ app/Livewire/Admin/Reports/ParentsReport.php
@@ -2,12 +2,30 @@
 
 namespace App\Livewire\Admin\Reports;
 
+use App\Models\EventRegistration;
 use Livewire\Component;
 
 class ParentsReport extends Component
 {
+    public $search = '';
     public function render()
     {
-        return view('livewire.admin.reports.parents-report');
-    }
+        // Query to fetch students with search functionality
+        $parents = EventRegistration::where('current_status', 2)
+        ->select('first_name', 'last_name', 'email', 'phone', 'referral_method')
+        ->when($this->search, function ($query) {
+         // Apply the search condition only if the search term is provided
+         $query->where(function ($subQuery) {
+             $subQuery->where('first_name', 'like', '%' . $this->search . '%')
+                      ->orWhere('last_name', 'like', '%' . $this->search . '%')
+                      ->orWhere('email', 'like', '%' . $this->search . '%')
+                      ->orWhere('phone', 'like', '%' . $this->search . '%')
+                     
+                      // Use || for string concatenation in SQLite
+                      ->orWhereRaw("first_name || ' ' || last_name LIKE ?", ['%' . $this->search . '%']);
+         });
+     })
+     ->get();
+         return view('livewire.admin.reports.parents-report', compact('parents'));
+     }
 }
