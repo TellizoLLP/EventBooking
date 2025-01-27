06e632af5cf9b05238b7839b10ad61cfa46a8500
NoufalTellizo <noufal@blarve.com>
2025-01-27 11:30:32 +0530

    Students Report

--- app/Livewire/Admin/Reports/StudentsReport.php
+++ app/Livewire/Admin/Reports/StudentsReport.php
@@ -2,12 +2,16 @@
 
 namespace App\Livewire\Admin\Reports;
 
+use App\Models\EventRegistration;
 use Livewire\Component;
 
 class StudentsReport extends Component
 {
     public function render()
     {
-        return view('livewire.admin.reports.students-report');
+        $students = EventRegistration::where('current_status', 1)
+            ->select('first_name', 'last_name', 'email', 'phone', 'school_name', 'school_grade', 'referral_method')
+            ->get();
+        return view('livewire.admin.reports.students-report', compact('students'));
     }
 }
