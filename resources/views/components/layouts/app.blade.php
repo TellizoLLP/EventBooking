3b7813e0237c0b38c106e050688e0a7720fb62b4
akshayxfortech <akshay@xfortech.com>
2025-01-31 17:08:31 +0530

    changes

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>{{ $title ?? 'Page Title' }}</title>
+    <title>{{ $title ?? 'Youth Medical Forum (YMF) 2025' }}</title>
 
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link rel="shortcut icon" href="{{asset('assets/img/icons/icon-48x48.png')}}" />
